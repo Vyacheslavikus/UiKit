5a1a21f823ff6c5f653700538d1f6c07bbf35927
Vyacheslavikus <lapshina90@mail.ru>
2024-02-23 17:01:42 +0300

    CorrectExtinsions

--- HomeWorkUK/HomeWorkUK/ViewController.swift
+++ HomeWorkUK/HomeWorkUK/ViewController.swift
@@ -5,56 +5,66 @@
 //  Created by Слава on 07.02.2024.
 //
 
-
 import UIKit
 
+extension UIViewController {
+
+    func presentAlert(withTitle title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default) { action in
+            print("You've pressed OK Button")
+        }
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+}
+
 class ViewController: UIViewController {
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
     }
-    
+
     @IBAction func setupUI() {
         view.backgroundColor = .white
-        
+
         let additionButton = UIButton(type: .system)
         additionButton.setTitle("Сложение", for: .normal)
         additionButton.addTarget(self, action: #selector(additionButtonTapped), for: .touchUpInside)
-        
+
         let guessNumberButton = UIButton(type: .system)
         guessNumberButton.setTitle("Угадай число", for: .normal)
         guessNumberButton.addTarget(self, action: #selector(guessNumberButtonTapped), for: .touchUpInside)
-        
+
         let stackView = UIStackView(arrangedSubviews: [additionButton, guessNumberButton])
         stackView.axis = .vertical
         stackView.spacing = 20
         stackView.distribution = .fillEqually
-        
+
         view.addSubview(stackView)
-        
+
         stackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
-    
+
     @objc func additionButtonTapped() {
         let alert = UIAlertController(title: "Сложение", message: "Введите два числа", preferredStyle: .alert)
-        
+
         alert.addTextField { textField in
             textField.placeholder = "Число 1"
             textField.keyboardType = .numberPad
         }
-        
+
         alert.addTextField { textField in
             textField.placeholder = "Число 2"
             textField.keyboardType = .numberPad
         }
-        
+
         alert.addAction(UIAlertAction(title: "Посчитать", style: .default, handler: { _ in
             if let textFields = alert.textFields, textFields.count == 2,
                let number1 = Double(textFields[0].text ?? ""),
@@ -62,24 +72,24 @@ class ViewController: UIViewController {
                 let sum = number1 + number2
                 self.showAlert(message: "Результат сложения: \(sum)")
             } else {
-                self.showAlert(message: "Введите корректные числа.")
+                self.presentAlert(withTitle: "Ошибка", message: "Введите корректные числа.")
             }
         }))
-        
+
         alert.addAction(UIAlertAction(title: "Отмена", style: .cancel, handler: nil))
-        
+
         present(alert, animated: true, completion: nil)
     }
-    
+
     @objc func guessNumberButtonTapped() {
         let randomNumber = Int.random(in: 1...100)
         let alert = UIAlertController(title: "Угадай число", message: "Введите число от 1 до 100", preferredStyle: .alert)
-        
+
         alert.addTextField { textField in
             textField.placeholder = "Введите число"
             textField.keyboardType = .numberPad
         }
-        
+
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
             if let enteredText = alert.textFields?.first?.text, let enteredNumber = Int(enteredText) {
                 if (1...100).contains(enteredNumber){
@@ -89,21 +99,22 @@ class ViewController: UIViewController {
                         self.showAlert(message: "Увы, правильное число было \(randomNumber). Попробуйте еще раз!")
                     }
                 } else {
-                    self.showAlert(message: "Введите корректное число.")
+                    self.presentAlert(withTitle: "Ошибка", message: "Введите корректное число.")
                 }
             }
         }))
-        
+
         alert.addAction(UIAlertAction(title: "Отмена", style: .cancel, handler: nil))
-        
+
         present(alert, animated: true, completion: nil)
     }
-    
+
     func showAlert(message: String) {
         let alert = UIAlertController(title: "Результат", message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
+
 }
 
 
