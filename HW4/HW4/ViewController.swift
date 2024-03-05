16953de126fca28adbe7166d2ad5441a71567f8e
Vyacheslavikus <lapshina90@mail.ru>
2024-03-05 14:03:42 +0300

    CommitWithWriteProject

---
+++ HW4/HW4/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HW4
+//
+//  Created by Слава on 25.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
--- HW4/UiKit/ViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ViewController.swift
-//  UiKit
-//
-//  Created by Слава on 04.02.2024.
-//
-
-import UIKit
-
